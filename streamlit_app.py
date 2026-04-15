c17b72fac8351767e090239c9607f4b5bcd960a1
Paul <183131260+SmilingSupernova@users.noreply.github.com>
2026-04-15 00:01:40 +0000

    Updated from template

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,48 +14,9 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("Input")
-    uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "png", "jpeg"])
-    
-    if uploaded_file is not None:
-        img = Image.open(uploaded_file)
-        # Display the uploaded image
-        st.image(img, caption="Original Image", use_container_width=True)
-    else:
-        # Placeholder box if no image is uploaded
-        st.info("Please drag and drop or upload an image to start.")
+    uploaded_file = st.file_uploader("Upload a legal document here...", type=["jpg", "png", "jpeg"])
+    st.info("Please drag and drop or upload a file to start.")
 
 with col2:
     st.subheader("Model Output")
-    if uploaded_file is not None:
-        text = st.empty()
-        for i in range(1):
-            text.text_input("Processing", key=f"{i}0{i}")
-            text.empty()
-            time.sleep(.5)
-            text.empty()
-            text.text_input("Processing .", key=f"{i}1{i}")
-            time.sleep(.5)
-            text.empty()
-            text.text_input("Processing . .", key=f"{i}2{i}")
-            time.sleep(.5)
-            text.empty()
-            text.text_input("Processing . . .", key=f"{i}3{i}")
-            time.sleep(.5)
-            text.empty()
-            text.text_input("Processing .. .", key=f"{i}4{i}")
-            time.sleep(.5)
-            text.empty()
-            text.text_input("Processing ...", key=f"{i}5{i}")
-            time.sleep(.5)
-            text.text_input("Processing .. .", key=f"{i}6{i}")
-            time.sleep(.5)
-            text.empty()
-            text.text_input("Processing . . .", key=f"{i}7{i}")
-            time.sleep(.5)
-
-        if(uploaded_file == "name of file 1"):
-            st.image(processedImage1, caption="Processed Image with Bounding Boxes", use_container_width=True)
-        else:
-            st.image(processedImage2, caption="Processed Image with Bounding Boxes", use_container_width=True)
-    else:
-        st.write("Results will appear here.")
+    st.write("Results will appear here.")
